3e652ef30816beaf1be6341311ea80532275e150
MasudMallik <masudmallik96@gmail.com>
2026-02-22 11:38:34 +0530

    __

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Form, BackgroundTasks
-from modules.email_send import send_otp
+from backend.modules.email_send import send_otp
 from upstash_redis import Redis
 from dotenv import load_dotenv
 import os
@@ -10,13 +10,15 @@ app = FastAPI()
 redis_ = Redis(url=os.getenv("redis_url"),token=os.getenv("redis_token"))
 
 async def otp_gen(email: str):
-    otp =await  send_otp(email)
+    otp = await send_otp(email)
     if otp:
         redis_.setex(f"otp:{email}", 300, str(otp))
+    else:
+        redis_.setex(f"otp:{email}", 300, None)
 
 @app.post("/")
 async def get_user(background_tasks: BackgroundTasks, email: str = Form(...)):
-    await send_otp(email)
+    background_tasks.add_task(otp_gen,email)
     return {"status": "OTP task scheduled"}
 
 @app.post("/reg")
