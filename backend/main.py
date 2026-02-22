a21443c4464e824d0d8054a3d0d662620dd18087
MasudMallik <masudmallik96@gmail.com>
2026-02-22 19:24:44 +0530

    __

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,11 @@ from modules.email_send import send_otp
 from upstash_redis import Redis
 from dotenv import load_dotenv
 import os
+import aiosmtplib
+from email.message import EmailMessage
+import random
+
+
 load_dotenv()
 
 
@@ -17,10 +22,39 @@ async def otp_gen(email: str):
         redis_.setex(f"otp:{email}", 300, None)
 
 @app.post("/")
-async def get_user(background_tasks: BackgroundTasks, email: str = Form(...)):
-    background_tasks.add_task(otp_gen,email)
-    ot=redis_.get(f"otp:{email}")
-    return {"status": f"OTP task scheduled and otp{ot}"}
+async def get_user( email: str = Form(...)):
+    message=EmailMessage()
+    message["From"]=os.getenv("name")
+    message["To"]=email
+    message["Subject"]="Predicta verification code"
+    r=random.randint(000000,999999)
+    try:
+        message.set_content(f"""Hello,
+
+            Welcome to Predicta! To complete your verification, please use the following one-time password (OTP):
+
+            🔑 Your OTP: {r}
+
+            This code will expire in 5 minutes. For your security, do not share this code with anyone.
+
+            If you did not request this verification, please ignore this message.
+
+            Thank you,
+            The Predicta Team
+            """)
+        await aiosmtplib.send(
+            message,
+            hostname=os.getenv("hostname"),
+            username=os.getenv("name"),
+            use_tls=True,
+            password=os.getenv("password")
+        )
+    except Exception:
+        return {"otp":"not send"}
+    else:
+        redis_.setex(f"otp:{email}",180,r)
+        return {"otp":r}
+
 
 @app.post("/reg")
 async def register(email: str = Form(...), otp: str = Form(...)):
